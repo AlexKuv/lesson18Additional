d6f1aa2026fd611ece450bad3634ac896e673833
Alexandr <kuvaldin.ai95@gmail.com>
2020-11-06 10:41:11 +0700

    3 часть дз 18 урока

--- script/script.js
+++ script/script.js
@@ -1,35 +1,45 @@
 'use strict';
 
-const input = document.querySelector('.input'),
-userText = document.querySelector('.user-text');
+const btnStart = document.querySelector('.start'),
+btnStop = document.querySelector('.stop'),
+btnReset = document.querySelector('.reset'),
+train = document.querySelector('.img'),
+buttons = document.querySelector('.buttons');
+console.log('buttons: ', buttons);
 
-const changeP = () => {
-  userText.textContent = input.value;
-};
+let count =0;
+let trainAnimate;
 
-// const throttle = (f, t) => {
-//   return function (args) {
-//     let previousCall = this.lastCall;
-//     this.lastCall = Date.now();
-//     if (previousCall === undefined || (this.lastCall - previousCall) > t) { 
-//       f(args);
-//     }
-//   };
-// };
-
-const debounce = (f, t) => {
-  return function (args) {
-    let previousCall = this.lastCall;
-    this.lastCall = Date.now();
-    if (previousCall && ((this.lastCall - previousCall) <= t)) {
-      clearTimeout(this.lastCallTimer);
-    }
-    this.lastCallTimer = setTimeout(() => f(args), t);
-  };
+let trainInterval;
+const step = () => {
+  trainAnimate = requestAnimationFrame(step);
+  count++;
+  train.style.left = count*2 + 'px';
+  if(count === 650){
+    cancelAnimationFrame(trainAnimate);
+  }
 };
 
 
-input.addEventListener('input',debounce(changeP,300));
-
-
+buttons.addEventListener('click', (e) => {
+  count++;
+  let target = e.target;
+  if(target.classList.contains('start')){
+    trainAnimate = requestAnimationFrame(step);
+    btnStart.classList.remove('start');
+    btnStart.textContent = 'Stop'; 
+  }else if (target.classList.contains('stop')){
+    cancelAnimationFrame(trainAnimate);
+    btnStart.classList.add('start');
+    btnStart.textContent = 'Start'; 
+  }else if (target.classList.contains('reset')){
+     cancelAnimationFrame(trainAnimate);
+   if(count>0){
+     train.style.left = 0;
+     count = 0;
+     btnStart.classList.add('start');
+    btnStart.textContent = 'Start'; 
+   }
+  }
+});
 
